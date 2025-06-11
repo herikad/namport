ec96dd1e61114ac4637a6546bdd77dfa97d99e3e
herikad <herika.desai@softqubes.com>
2025-06-11 16:39:26 +0530

    remove unneccessory code

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -5,12 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Department;
 use App\Models\MstClient;
-use App\Models\User;
-use Helper;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 use Log;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Validator;
 
 

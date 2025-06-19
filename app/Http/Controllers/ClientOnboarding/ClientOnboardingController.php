be4d202beeab8867b610aa59d290d85b593b7f17
herikad <herika.desai@softqubes.com>
2025-06-19 18:40:54 +0530

    voice profile testing

--- app/Http/Controllers/ClientOnboarding/ClientOnboardingController.php
+++ app/Http/Controllers/ClientOnboarding/ClientOnboardingController.php
@@ -110,7 +110,13 @@ class ClientOnboardingController extends Controller
                     ], 200);
                 }
                 // Get public URL
-                Storage::disk(config('filesystems.default'))->put("voice_profiles/{$mp3Filename}", file_get_contents($mp3Path));
+                // Storage::disk(config('filesystems.default'))->put("voice_profiles/{$mp3Filename}", file_get_contents($mp3Path));
+                Storage::disk(config('filesystems.default'))->putFileAs(
+                          'voice_profiles',
+                          File($mp3Path),
+                          $mp3Filename,
+                          ['visibility' => 'public', 'ContentType' => 'audio/mpeg']
+                      );
 
                 // Storage::disk(config('filesystems.default'))->put(
                 //     "voice_profiles/{$mp3Filename}",
